96f3b3bfc7ae2649be5a16525bcddd6a8847b172
ahmad-haggag <ahmad.haggag1982@gmail.com>
2022-09-29 14:19:58 +0200

    glue job deployment

---
+++ glue-jobs/etl-sample.py
@@ -0,0 +1,21 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from pyspark.sql.functions import col,year,month,dayofmonth,to_date
+
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+print(args)
+
+
+job.commit()
